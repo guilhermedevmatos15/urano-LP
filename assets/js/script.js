6315ca30d88a26a6894b2d5bd5fccae046a8b7ff
guilhermedevmatos15 <devguilhermeferreira@gmail.com>
2024-10-27 10:59:26 -0300

    Lógica do menu feita

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,28 @@ import animationsInit from "./animations.js";
 
 // Inicializando animações
 animationsInit();
+
+// Lógica do menu
+(() => {
+   const menu = document.querySelector('.menu');
+   const $activeMenuElements = [...document.querySelectorAll('[data-menu="on"]')];
+   const $disableMenuElements = [...document.querySelectorAll('[data-menu="off"]')];
+
+   $activeMenuElements.forEach((element) => {
+      element.addEventListener('click', () => {
+         menu.classList.add('active');
+      });
+   });
+
+   $disableMenuElements.forEach((element) => {
+      element.addEventListener('click', () => {
+         menu.classList.remove('active');
+      });
+   });
+   
+   window.addEventListener('resize', () => {
+      if (window.innerWidth >= 900) {
+         menu.classList.remove('active');
+      }
+   });
+})();
